0c8615d1b16cfbd5ab1c10f8b662a55c6f9d9d18
hyemin <hhm930415@gmail.com>
2021-04-22 19:35:33 +0900

    UNION

---
+++ 4-3_UNION.sql
@@ -0,0 +1,57 @@
+/* UNION, UNION ALL : 합집합 처리
+-- UNION : 중복데이터를 제거하고 하나만 표시형태로 합해줌
+-- UNION ALL : 중복테이터 포함해서 합해줌
+단, 조회하는 컬럼의 이름, 순서, 갯수, 타입이 일치하도록 작성
+*******************************/
+
+-- UNION 사용 예제
+SELECT CUSTID, NAME FROM CUSTOMER
+WHERE CUSTID IN (1, 2, 3);
+
+SELECT CUSTID, NAME FROM CUSTOMER 
+WHERE CUSTID IN (3, 4, 5);
+
+-- UNION으로 합하기(중복데이터 하나만 표시)
+SELECT CUSTID, NAME FROM CUSTOMER
+WHERE CUSTID IN (1, 2, 3)
+UNION
+SELECT CUSTID, NAME FROM CUSTOMER 
+WHERE CUSTID IN (3, 4, 5);
+
+-- UNION ALL으로 합하기(중복데이터 모두 표시(사용))
+SELECT CUSTID, NAME FROM CUSTOMER
+WHERE CUSTID IN (1, 2, 3)
+UNION ALL
+SELECT CUSTID, NAME FROM CUSTOMER 
+WHERE CUSTID IN (3, 4, 5)
+ORDER BY NAME; -- ORDER BY 절은 맨 마지막에 사용.
+
+------------------------------------
+-- MINUS : 차집합 ( 빼기 연산) -> 중복데이터 제거
+SELECT CUSTID, NAME FROM CUSTOMER
+WHERE CUSTID IN (1, 2, 3)
+MINUS
+SELECT CUSTID, NAME FROM CUSTOMER 
+WHERE CUSTID IN (3, 4, 5);
+
+---------------------------------------
+-- INTERSECT : 교집합 -> 중복된 데이터만 추출(조회)
+---> 조인문의 동등비교 결과와 동일
+SELECT CUSTID, NAME FROM CUSTOMER
+WHERE CUSTID IN (1, 2, 3)
+INTERSECT
+SELECT CUSTID, NAME FROM CUSTOMER 
+WHERE CUSTID IN (3, 4, 5);
+--- 조인문(JOIN문)
+SELECT * 
+FROM (SELECT CUSTID, NAME FROM CUSTOMER WHERE CUSTID IN (1, 2, 3)) A,
+    (SELECT CUSTID, NAME FROM CUSTOMER WHERE CUSTID IN (3, 4, 5)) B
+WHERE A.CUSTID = B.CUSTID
+;
+
+
+
+
+
+
+
